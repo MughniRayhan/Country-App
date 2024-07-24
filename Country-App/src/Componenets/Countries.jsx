4375ce0b3f048d4a16ce5b9bd3aad428116157b3
MughniRayhan <tishtisha13@gmail.com>
2024-07-24 23:53:59 +0600

    dispaly datas

--- Country-App/src/Componenets/Countries.jsx
+++ Country-App/src/Componenets/Countries.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {v4 as uuidv4} from "uuid";
+import Country from './Country';
 
 function Countries(props) {
+    
   return (
     <section>
         {/* mapping data */}
         {props.countries.map((country)=> {
             const countryNew = {country, id:uuidv4()}
+            return <Country {...countryNew} key={countryNew.id}/>
         })}
     </section>
   )
