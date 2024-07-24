29bc3125d45646ad0faa7222e5d9279885e3e8e1
MughniRayhan <tishtisha13@gmail.com>
2024-07-24 22:00:06 +0600

    create useState variables

--- Country-App/src/App.jsx
+++ Country-App/src/App.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+const url = "https://restcountries.com/v3.1/all"
 function App() {
+
+  const [isLoading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [countries, setCountries] = useState([])
+
   return (
     <div>App</div>
   )
