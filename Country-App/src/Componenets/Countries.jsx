a6513888ceb4a38cb4f4681d6da5dfa3f53a85b5
MughniRayhan <tishtisha13@gmail.com>
2024-07-24 23:27:01 +0600

    mapping data

--- Country-App/src/Componenets/Countries.jsx
+++ Country-App/src/Componenets/Countries.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import {v4 as uuidv4} from "uuid";
 
 function Countries(props) {
   return (
     <section>
-       
+        {/* mapping data */}
+        {props.countries.map((country)=> {
+            const countryNew = {country, id:uuidv4()}
+        })}
     </section>
   )
 }
